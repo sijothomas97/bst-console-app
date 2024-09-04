36399c32454b2f4d1e543a1ecf7ad7c2072195a9
Sijo Thomas <164408829+sijothomas97@users.noreply.github.com>
2024-09-04 23:26:35 +0100

    Build project structure

---
+++ src/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskA
+{
+    public class Node
+    {
+        private string data;
+        public Node Left, Right;
+
+        public Node(string item)
+        {
+            data = item;
+            Left = null;
+            Right = null;
+        }
+
+        public string Data
+        {
+            set { data = value; }
+            get { return data; }
+        }
+    }
+}
