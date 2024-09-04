7d04e2c0e000f55311eacec0d2aa7b19333e5ec6
Sijo Thomas <164408829+sijothomas97@users.noreply.github.com>
2024-09-04 23:27:31 +0100

    Build project structure

---
+++ src/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace TaskA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int searchItem;
+            BinTree tree = new BinTree();
+            tree.InsertItem("Sijo", ref tree);
+            tree.InsertItem("Sona", ref tree);
+            tree.InsertItem("Paapu", ref tree);
+            tree.InsertItem("Minu", ref tree);
+            tree.InsertItem("Thomas", ref tree);
+            tree.InsertItem("Bro", ref tree);
+
+            Console.WriteLine("In-Order");
+            tree.InOrder();
+            Console.WriteLine("Pre-Order");
+            tree.PreOrder();
+            Console.WriteLine("Post-Order");
+            tree.PostOrder();
+            Console.WriteLine("------------------");
+            Console.WriteLine(tree.Longest());
+            Console.WriteLine("------------------");
+            Console.WriteLine(tree.Ancestor("Bro", "Thomas"));
+            //Console.WriteLine("Enter an element to search: ");
+            //searchItem = Convert.ToInt32(Console.ReadLine());
+            //if (tree.Contains(searchItem))
+            //{
+            //    Console.WriteLine("Item found from the tree");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Item could not found from the tree");
+            //}
+
+            //Console.WriteLine("Enter an element to search: ");
+            //searchItem = Convert.ToInt32(Console.ReadLine());
+            //if (tree.Contains(searchItem))
+            //{
+            //    Console.WriteLine("Item found from the tree");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Item could not found from the tree");
+            //}
+        }
+    }
+}
